683722c6ed40d7325eb0afc4f0551891231b27d2
gconceicao1 <gabrielmoraesd@gmail.com>
2020-05-07 20:16:34 -0300

    ajustes

--- produto.js
+++ produto.js
@@ -17,16 +17,19 @@ app.get('/produtos', function(req,res){
         'nome':'processador intel i9',
         'preco':900,
         'peso': 0.50,
+        'img':"../webApi/img/i9.jpg"
     },
     {
         'nome':'Placa de Vídeo PCYes AMD Radeon R7 240, 2GB, GDDR5 - PJ240R712802D5',
         'preco':369.90,
-        'peso': 580
+        'peso': 580,
+        'img':"../webApi/img/placadevideo.png"
     },
     {
         'nome':'Mouse Gamer Razer Deathadder Elite Chroma Mecânico 16.000 DPI - RZ01-02010100-R3U1',
         'preco':284.90,
-        'peso': 240
+        'peso': 240,
+        'img':"../webApi/img/mouse.jpg"
     }
     ]
     
